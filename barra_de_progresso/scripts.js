f758e81956842d4cfaeb40329d72c035ee681155
Elias <lyusk8@gmail.com>
2023-06-25 12:17:07 -0300

    Refatora o javascript

--- barra_de_progresso/scripts.js
+++ barra_de_progresso/scripts.js
@@ -8,19 +8,17 @@ function updateProgressBar(){
     progressBar.style.width = progress + "%";
 }
 
-function nextStep(){
-    progress += 10;
-    if(progress > 100) progress = 100;
-    updateProgressBar();
-    console.log(progress)
+function nextPreviousStep(e){
+    if(e.target.id === "previous-btn"){
+        progress -= 10;
+        if(progress < 0) progress = 0;
+        updateProgressBar()
+    } else if(e.target.id === "next-btn"){
+        progress += 10;
+        if(progress > 100) progress = 100;
+        updateProgressBar();
+    }
 }
 
-function previousStep(){
-    progress -= 10;
-    if(progress < 0) progress = 0;
-    updateProgressBar()
-    console.log(progress)
-}
-
-nextBtn.addEventListener("click", nextStep);
-previousBtn.addEventListener("click", previousStep);
+nextBtn.addEventListener("click", nextPreviousStep);
+previousBtn.addEventListener("click", nextPreviousStep);
