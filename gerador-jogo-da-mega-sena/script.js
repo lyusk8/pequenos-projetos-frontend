cf193dc4ece8a21301a318d0fe0831b57a4fedc2
Elias <lyusk8@gmail.com>
2023-09-07 10:48:49 -0300

    Implementa o javaScript

---
+++ gerador-jogo-da-mega-sena/script.js
@@ -0,0 +1,20 @@
+const numeros = document.querySelectorAll(".number");
+const botao = document.querySelector("#generate")
+
+botao.addEventListener("click", ()=>{
+    const numerosSorteados = [];
+    for(let i = 0; i < numeros.length; i++){
+        const jogo = Math.floor(Math.random()*60)+1;
+        if(numerosSorteados.includes(jogo)){
+            i--
+            continue
+        } else {
+            numerosSorteados.push(jogo);
+        }
+
+    }
+    console.log(numerosSorteados.sort())
+    numeros.forEach((numero, i) => {
+        numero.textContent = numerosSorteados[i++];
+    })
+})
